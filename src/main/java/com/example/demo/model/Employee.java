b3ba20eefcfdc26590926f749ff15318b8c43f26
SaurabhTilak <saurbhtilak6@gmail.com>
2025-10-30 11:42:17 +0530

    This is Employee model class

---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,51 @@
+package com.example.demo.model;
+
+public class Employee {
+	private int id;
+	private String name;
+	private String email;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getSalary() {
+		return salary;
+	}
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+
+	private String contact;
+	private String salary;
+
+}
