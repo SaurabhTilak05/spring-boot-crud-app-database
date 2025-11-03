379fb6b5bb47442a92acc485113fb65e0a163683
SaurabhTilak <saurbhtilak6@gmail.com>
2025-11-03 09:58:35 +0530

    Employee Repository

--- src/main/java/com/example/demo/repository/EmpRepoImp.java
+++ src/main/java/com/example/demo/repository/EmpRepoImp.java
@@ -50,7 +50,42 @@ public class EmpRepoImp implements EmpRepoInf{
 				return emp;
 			}
 		});
-		return list;
+		return list.size()>0?list:null;
+	}
+
+	@Override
+	public Employee searchEmp(int id) {
+		List<Employee> list=template.query("select *from employee where id=?",new RowMapper<Employee>() {
+
+			@Override
+			public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+				 Employee e = new Employee();
+	                e.setId(rs.getInt("id"));
+	                e.setName(rs.getString("name"));
+	                e.setEmail(rs.getString("email"));
+	                e.setContact(rs.getString("contact"));
+	                e.setSalary(rs.getString("salary"));
+	                return e;
+			}
+		}, id
+		);
+		return list.isEmpty() ? null : list.get(0);
+		
+		
+	}
+
+	@Override
+	public Employee updateEmp(int id, Employee emp) {
+		
+		int value=template.update("UPDATE employee SET name = ?, email = ?, contact = ?, salary=?  WHERE id = ?", new Object[] {emp.getName(), emp.getEmail(),emp.getContact(),emp.getSalary(),id});
+		return value>0 ? emp:null;
+	
+	}
+
+	@Override
+	public boolean isDelete(int id) {
+		int value=template.update("delete from employee where id=?",+id);
+		return value>0 ?true:false;
 	}
 	
 }
--- src/main/java/com/example/demo/repository/EmpRepoInf.java
+++ src/main/java/com/example/demo/repository/EmpRepoInf.java
@@ -8,4 +8,7 @@ public interface EmpRepoInf {
 
 		public boolean isSave(Employee employee);
 		public List<Employee> getEmployee();
+		public Employee searchEmp(int id);
+		public Employee updateEmp(int id,Employee emp);
+		public boolean isDelete(int id);
 }
